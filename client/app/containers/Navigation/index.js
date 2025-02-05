f5de41be94db1924dc709bb47f7b61ac922d441d
Sanjan B M <acharyasanjan931@gmail.com>
2025-02-05 20:15:52 +0530

    add title

--- client/app/containers/Navigation/index.js
+++ client/app/containers/Navigation/index.js
@@ -180,7 +180,7 @@ class Navigation extends React.PureComponent {
                   />
                 )}
                 <Link to='/'>
-                  <h1 className='logo'>MERN Store</h1>
+                  <h1 className='logo'>FromToTable</h1>
                 </Link>
               </div>
             </Col>
